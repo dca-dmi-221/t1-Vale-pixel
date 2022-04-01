9a75571958d26b02e14c588ef8cd10b9b3e52e86
Vale-pixel <100896964@u.icesi.edu.co>
2022-03-31 23:23:11 -0500

    Crea arreglo de canciones.

---
+++ Songs.js
@@ -0,0 +1,17 @@
+class Songs {
+  constructor() {
+    this.songs = [];
+  }
+
+  newSong({ name, artist, img, duration, x, y, file }) {
+    let print = new Song({ name, artist, img, duration, x, y, file });
+    this.songs.push(print);
+    return print;
+  }
+  get allSongs() {
+    return this.songs;
+  }
+  get numberOfSongs() {
+    return this.songs.length;
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
-function setup() {
-  createCanvas(400, 400);
+let imgBackground;
+let player = new Player();
+
+function preload() {
+ 
+  //player.preloadSong();
+ 
 }
 
+function setup() {
+  let canva = createCanvas(1098, 627);
+ canva.mousePressed(canvasPressed);
+ // imgBackground = createImg("resources/background.png");
+ // imgBackground.position(0, 0);
+ 
+  player.arrayOfSongs();
+  // database.doSomething();
+}
 function draw() {
-  background(220);
+ // imgBackground;
+}
+function canvasPressed() {
+  player.playSong();
 }
